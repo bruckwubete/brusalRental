8bdaedfd0701c953a55ba0725eb04a05838e2a5b
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2016-09-02 23:59:51 -0400

    commit

--- app/scripts/controllers/showview.js
+++ app/scripts/controllers/showview.js
@@ -11,7 +11,7 @@ angular.module('dvdRentalFrontendApp')
   .controller('ShowviewCtrl', function ($state, lubTmdbApi) {
     var vm = this;
     vm.getShow = getShow;
-    vm.show = {};
+    vm.queryResult = [];
     activate();
     function activate(){
       getShow();
@@ -19,6 +19,7 @@ angular.module('dvdRentalFrontendApp')
 
     function getShow(){
         exec('tv','tv',$state.params.id)
+        exec('tv','credits',$state.params.id)
     }
 
     function exec (type, method, query) {
@@ -28,10 +29,15 @@ angular.module('dvdRentalFrontendApp')
     }
 
  function  suc (result){
-       console.log(result);
-       vm.show = result.data;
+   vm.queryResult.push(result.data);
+   vm.show = vm.queryResult[0];
+   if(vm.queryResult[1]){
+     vm.cast = vm.queryResult[1].cast;
+     vm.crew = vm.queryResult[1].crew;
+   }
    };
 
  function err (results) {
+   console.log(results);
    }
   });
