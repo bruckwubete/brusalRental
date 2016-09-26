bc8b07f598e46229f7ccecb64f4a56d30e1d25ed
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2016-09-26 18:58:56 +0000

    Revert "to revert"
    This reverts commit f57d523742cce473ae15b66d51b4929b5178bc4a.

--- gulpfile.js
+++ gulpfile.js
@@ -81,14 +81,14 @@ gulp.task('clean:tmp', function (cb) {
 });
 
 gulp.task('start:client', ['start:server', 'styles', 'lint:scripts'], function () {
-  openURL('http://localhost:8080');
+  openURL('http://localhost:9000');
 });
 
 gulp.task('start:server', function() {
   $.connect.server({
     root:[yeoman.temp, yeoman.app],
     livereload:true,
-    port: 8080,
+    port: 9000,
     middleware:function(connect, opt){
       return [['/bower_components',
         connect["static"]('./bower_components')]]
