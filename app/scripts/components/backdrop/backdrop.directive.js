01360c07df2e2001d7ecdf750ec94dbea35abb46
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2016-09-30 00:28:41 -0400

    Revert "to revert"
    This reverts commit d20d6d91da3f120ba6faf87452720846ad3f5d15.

--- app/scripts/components/backdrop/backdrop.directive.js
+++ app/scripts/components/backdrop/backdrop.directive.js
@@ -5,18 +5,7 @@
   angular
     .module('dvdRentalFrontendApp')
     // John Papa [Style Y073] Unique directive prefix mdb
-    .directive('mdbBackdrop', mdbBackdrop)
-    .directive('backgroundImage', function(){
-	return function(scope, element, attrs){
-		restrict: 'A',
-		attrs.$observe('backgroundImage', function(value) {
-			element.css({
-				'background': 'url(' +'https://image.tmdb.org/t/p/w1920' + value +')',
-        'background-size': '100% 100%'
-			});
-		});
-	};
-});
+    .directive('mdbBackdrop', mdbBackdrop);
 
   mdbBackdrop.$inject = []
 
